4b3a4f08a896ddcbc976ba0dbf359e67482ea51c
KMIS2008 <129222339+KMIS2008@users.noreply.github.com>
2025-02-04 14:52:41 +0200

    update app

--- app.js
+++ app.js
@@ -1,36 +1,17 @@
 const express = require ("express");
-const morgan = require ("morgan");
 const cors =require ("cors");
-const Visit=require ("./model/visits");
-
-const visitsRouter = require ("./routes/visitsRouter.js");
-const emailRouter=require('./routes/emailRouter.js');
-const checkRouter=require('./routes/checkRouter.js');
-const serviceRouter=require('./routes/serviceRouter.js');
-const feedbackRouter = require('./routes/feedbackRouter.js');
 
 // const authRouter = require('./routes/authRouter.js');
 
-
-const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
-const {DB_HOST} = process.env;
-mongoose.set('strictQuery', true);
+
 
 const app = express();
 
 app.use(cors());
-app.use(morgan("tiny"));
 
 app.use(express.json());
-
-
-app.use("/api/visits", visitsRouter);
-app.use("/api/email", emailRouter);
-app.use("/api/check-password", checkRouter);
-app.use("/api/service", serviceRouter);
-app.use("/api/feedback", feedbackRouter);
 
 
 // app.use("/api/user", authRouter);
@@ -45,21 +26,7 @@ app.use((err, req, res, next) => {
   res.status(status).json({ message });
 });
 
-// const initializeCounter = async () => {
-//   const existing = await Visit.findOne();
-//   if (!existing) {
-//     await new Visit({ count: 0 }).save();
-//   }
-// };
-// initializeCounter();
 
-mongoose.connect(DB_HOST)
-.then(()=>{
-  console.log('Database connection successful');
-  app.listen(3002, () => {
+  app.listen(3003, () => {
   console.log("Server is running. Use our API on port: 3002");
-})})
-.catch(error=>{
-  console.log(error.message)
-  process.exit(1)
 })
